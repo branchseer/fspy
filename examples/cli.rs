f29c2d85a4a3156cd9da6999b398111137e82f95
branchseer <3612422+branchseer@users.noreply.github.com>
2025-04-13 16:27:04 +0000

    update

--- examples/cli.rs
+++ examples/cli.rs
@@ -1,13 +1,14 @@
 use std::{
+    ffi::c_void,
     io::{self, Write},
-    mem::offset_of,
+    mem::{offset_of, zeroed},
     os::{
         fd::{AsRawFd, FromRawFd, IntoRawFd},
         unix::net::UnixStream as StdUnixStream,
     },
 };
 
-use libc::c_int;
+use libc::{c_int, memset, SECCOMP_GET_NOTIF_SIZES};
 use passfd::{tokio::FdPassingExt as _, FdPassingExt as _};
 use tokio::{
     io::{unix::AsyncFd, AsyncReadExt},
@@ -25,9 +26,68 @@ fn check_nonnegative(ret: c_int) -> io::Result<()> {
 unsafe fn seccomp(
     operation: libc::c_uint,
     flags: libc::c_uint,
-    args: *const libc::c_void,
+    args: *mut libc::c_void,
 ) -> libc::c_int {
     return c_int::try_from(libc::syscall(libc::SYS_seccomp, operation, flags, args)).unwrap();
+}
+
+struct SeccompNotifyBuf {
+    req: *mut c_void,
+    req_size: usize,
+    resp: *mut c_void,
+    resp_size: usize,
+}
+unsafe impl Send for SeccompNotifyBuf {}
+impl SeccompNotifyBuf {
+    pub fn alloc() -> io::Result<Self> {
+        let mut sizes = unsafe { zeroed::<libc::seccomp_notif_sizes>() };
+        check_nonnegative(unsafe { seccomp(SECCOMP_GET_NOTIF_SIZES, 0, (&raw mut sizes).cast()) })?;
+        let req_size = sizes.seccomp_notif as usize;
+        // TODO: use global allocator (make sure the alignment is correct)
+        let req = unsafe { libc::malloc(req_size) };
+        if req.is_null() {
+            return Err(io::Error::last_os_error());
+        }
+        // check the example in https://man7.org/linux/man-pages/man2/seccomp_unotify.2.html
+        let resp_size = libc::size_t::max(
+            size_of::<libc::seccomp_notif_resp>(),
+            sizes.seccomp_notif_resp as _,
+        );
+        let resp = unsafe { libc::malloc(resp_size) };
+        if resp.is_null() {
+            return Err(io::Error::last_os_error());
+        }
+        Ok(Self {
+            req,
+            req_size,
+            resp,
+            resp_size,
+        })
+    }
+    pub fn zeroed(&mut self) -> (&mut libc::seccomp_notif, &mut libc::seccomp_notif_resp) {
+        unsafe { self.req.write_bytes(0, self.req_size) };
+        unsafe { self.resp.write_bytes(0, self.resp_size) };
+        unsafe {
+            (
+                self.req
+                    .cast::<libc::seccomp_notif>()
+                    .as_mut()
+                    .unwrap_unchecked(),
+                self.resp
+                    .cast::<libc::seccomp_notif_resp>()
+                    .as_mut()
+                    .unwrap_unchecked(),
+            )
+        }
+    }
+}
+impl Drop for SeccompNotifyBuf {
+    fn drop(&mut self) {
+        unsafe {
+            libc::free(self.req);
+            libc::free(self.resp)
+        }
+    }
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -92,7 +152,7 @@ async fn main() -> io::Result<()> {
                 ),
             ];
 
-            let prog = libc::sock_fprog {
+            let mut prog = libc::sock_fprog {
                 len: filter.len() as _,
                 filter: filter.as_mut_ptr(),
             };
@@ -100,7 +160,7 @@ async fn main() -> io::Result<()> {
             let notify_fd = seccomp(
                 libc::SECCOMP_SET_MODE_FILTER,
                 libc::SECCOMP_FILTER_FLAG_NEW_LISTENER as _,
-                ((&prog) as *const libc::sock_fprog).cast(),
+                (&raw mut prog).cast(),
             );
             if notify_fd == -1 {
                 return Err(io::Error::last_os_error());
@@ -123,12 +183,30 @@ async fn main() -> io::Result<()> {
         const SECCOMP_IOCTL_NOTIF_ID_VALID: libc::c_ulong = 1074274562;
         const SECCOMP_IOCTL_NOTIF_SEND: libc::c_ulong = 3222806785;
 
-        let n = libc::seccomp_notif { };
+        let mut notify_buf = SeccompNotifyBuf::alloc()?;
 
         loop {
+            let (req, resp) = notify_buf.zeroed();
             let readable_nofity = async_notify.readable_mut().await?;
-            let ret = unsafe { libc::ioctl(*readable_nofity.get_inner(), SECCOMP_USER_NOTIF_FLAG_CONTINUE) };
-            
+            let ret = unsafe { libc::ioctl(*readable_nofity.get_inner(), SECCOMP_IOCTL_NOTIF_RECV, &raw mut *req) };
+            if ret < 0 && unsafe { *libc::__errno_location() } == libc::EINTR {
+                continue;
+            }
+            check_nonnegative(ret)?;
+
+            dbg!((req.data.nr, libc::SYS_mkdir));
+
+            resp.id = req.id;
+            resp.flags = libc::SECCOMP_USER_NOTIF_FLAG_CONTINUE as _;
+            let writable_notify = async_notify.writable_mut().await?;
+            let ret = unsafe { libc::ioctl(*writable_notify.get_inner(), SECCOMP_IOCTL_NOTIF_SEND, &raw mut *resp) };
+            if ret < 0 && unsafe { *libc::__errno_location() } == libc::ENOENT {
+                continue;
+            } 
+            check_nonnegative(ret)?;
+
+            eprintln!("resp sent");
+
             break;
         }
         io::Result::Ok(())
