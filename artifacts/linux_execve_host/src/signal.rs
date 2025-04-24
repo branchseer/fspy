5fe93e68fa7e8c3a6ff211cece3d1ff56dd74513
branchseer <dk4rest@gmail.com>
2025-04-24 16:26:57 +0800

    aarch64 condition

--- artifacts/linux_execve_host/src/signal.rs
+++ artifacts/linux_execve_host/src/signal.rs
@@ -3,7 +3,7 @@ use std::{
     io::{self, IoSlice},
 };
 
-use crate::{client::global_client, PATH_MAX};
+use crate::{PATH_MAX, client::global_client};
 use arrayvec::ArrayVec;
 use libc::c_char;
 use linux_raw_sys::general as linux_sys;
@@ -30,56 +30,59 @@ extern "C" fn handle_sigsys(
     // }
     let ucontext = unsafe { data.cast::<libc::ucontext_t>().as_mut().unwrap_unchecked() };
     // aarch64
-    let regs = &mut ucontext.uc_mcontext.regs;
-    let sysno = regs[8] as u32;
-    let client = unsafe { global_client() };
-    match sysno {
-        linux_sys::__NR_openat => {
-            let path_ptr = regs[1] as *const c_char;
-            let path = unsafe { CStr::from_ptr(path_ptr) }.to_bytes();
+    #[cfg(target_arch = "aarch64")]
+    {
+        let regs = &mut ucontext.uc_mcontext.regs;
+        let sysno = regs[8] as u32;
+        let client = unsafe { global_client() };
+        match sysno {
+            linux_sys::__NR_openat => {
+                let path_ptr = regs[1] as *const c_char;
+                let path = unsafe { CStr::from_ptr(path_ptr) }.to_bytes();
 
-            client
-                .ipc_socket
-                .send_vectored(&[
-                    // IoSlice::new( slice::from_ref(&(AccessKind::Open.into()))),
-                    IoSlice::new(path),
-                ])
-                .unwrap();
-            regs[0] = unsafe {
-                libc::syscall(
-                    linux_sys::__NR_openat as _,
-                    regs[0],
-                    regs[1],
-                    regs[2],
-                    regs[3],
-                    SYSCALL_MAGIC,
-                )
-            } as _;
+                client
+                    .ipc_socket
+                    .send_vectored(&[
+                        // IoSlice::new( slice::from_ref(&(AccessKind::Open.into()))),
+                        IoSlice::new(path),
+                    ])
+                    .unwrap();
+                regs[0] = unsafe {
+                    libc::syscall(
+                        linux_sys::__NR_openat as _,
+                        regs[0],
+                        regs[1],
+                        regs[2],
+                        regs[3],
+                        SYSCALL_MAGIC,
+                    )
+                } as _;
+            }
+            linux_sys::__NR_execve => {
+                let program = regs[0] as *const c_char;
+                let argv = regs[1] as *const *const c_char;
+                let envp = regs[2] as *const *const c_char;
+
+                let mut program_env_buf =
+                    ArrayVec::<u8, { ENVNAME_PROGRAM.len() + 1 + PATH_MAX + 1 }>::new();
+                let mut envp_buf = ArrayVec::<*const c_char, 1024>::new();
+
+                // unsafe {
+                //     global_state.prepare_envp(program, envp, &mut program_env_buf, &mut envp_buf)
+                // };
+
+                // regs[0] = unsafe {
+                //     libc::syscall(
+                //         linux_sys::__NR_execve as _,
+                //         global_state.host_path_env.value().as_ptr(),
+                //         argv,
+                //         envp_buf.as_ptr(),
+                //         SYSCALL_MAGIC,
+                //     )
+                // } as _;
+            }
+            _ => {}
         }
-        linux_sys::__NR_execve => {
-            let program = regs[0] as *const c_char;
-            let argv = regs[1] as *const *const c_char;
-            let envp = regs[2] as *const *const c_char;
-
-            let mut program_env_buf =
-                ArrayVec::<u8, { ENVNAME_PROGRAM.len() + 1 + PATH_MAX + 1 }>::new();
-            let mut envp_buf = ArrayVec::<*const c_char, 1024>::new();
-
-            // unsafe {
-            //     global_state.prepare_envp(program, envp, &mut program_env_buf, &mut envp_buf)
-            // };
-
-            // regs[0] = unsafe {
-            //     libc::syscall(
-            //         linux_sys::__NR_execve as _,
-            //         global_state.host_path_env.value().as_ptr(),
-            //         argv,
-            //         envp_buf.as_ptr(),
-            //         SYSCALL_MAGIC,
-            //     )
-            // } as _;
-        }
-        _ => {}
     }
 }
 
