d5216c1a453b1358b47d5d89d21279d4369de41b
branchseer <dk4rest@gmail.com>
2025-06-04 07:50:35 -0700

    init windows implementation

---
+++ src/windows/mod.rs
@@ -0,0 +1,11 @@
+// use std::process::Child;
+// use std::os::windows::process::ChildExt as _;
+use tokio::process::Child;
+
+use detours_sys2::DetourAttach;
+// use tokio::process::Child;
+
+
+pub fn spawn() {
+
+}
