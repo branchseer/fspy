b618385c3633c15103f84644cdf052523fbd1aec
branchseer <dk4rest@gmail.com>
2025-07-29 12:03:41 +0000

    dynamically get ld.so path in test

--- crates/fspy_shared_unix/src/spawn/elf.rs
+++ crates/fspy_shared_unix/src/spawn/elf.rs
@@ -4,19 +4,33 @@ use std::{
     path::Path,
 };
 
+use bstr::BStr;
 use elf::{ElfBytes, abi::PT_INTERP, endian::AnyEndian};
 
 pub fn is_dynamically_linked_to_libc(executable: impl AsRef<[u8]>) -> nix::Result<bool> {
-    let elf = ElfBytes::<'_, AnyEndian>::minimal_parse(executable.as_ref()).map_err(|_| nix::Error::ENOEXEC)?;
-    let Some(headers) = elf.segments() else {
+    let executable = executable.as_ref();
+    let Some(interp) = get_interp(executable)? else {
         return Ok(false);
+    };
+    let Some(interp_filename) = Path::new(OsStr::from_bytes(interp)).file_name() else {
+        return Ok(false);
+    };
+    let interp_filename = interp_filename.as_bytes();
+    Ok(interp_filename.starts_with(b"ld-") || interp_filename.starts_with(b"ld."))
+}
+
+fn get_interp(executable: &[u8]) -> nix::Result<Option<&BStr>> {
+    let elf = ElfBytes::<'_, AnyEndian>::minimal_parse(executable.as_ref())
+        .map_err(|_| nix::Error::ENOEXEC)?;
+    let Some(headers) = elf.segments() else {
+        return Ok(None);
     };
 
     let Some(interp_header) = headers
         .into_iter()
         .find(|header| header.p_type == PT_INTERP)
     else {
-        return Ok(false);
+        return Ok(None);
     };
     let Ok(interp) = elf.segment_data(&interp_header) else {
         return Err(nix::Error::ENOEXEC);
@@ -25,38 +39,29 @@ pub fn is_dynamically_linked_to_libc(executable: impl AsRef<[u8]>) -> nix::Resul
     let interp = CStr::from_bytes_until_nul(interp)
         .map(CStr::to_bytes)
         .unwrap_or(interp);
-
-    let Some(interp_filename) = Path::new(OsStr::from_bytes(interp)).file_name() else {
-        return Ok(false);
-    };
-    let interp_filename = interp_filename.as_bytes();
-    Ok(interp_filename.starts_with(b"ld-") || interp_filename.starts_with(b"ld."))
+    Ok(Some(BStr::new(interp)))
 }
 
 #[cfg(test)]
 mod tests {
-    use std::fs::{read_dir, read};
+    use std::fs::{read, read_dir};
 
     use super::*;
     #[test]
     fn dynamic_executable() {
-        assert_eq!(is_dynamically_linked_to_libc(read("/bin/cat").unwrap()).unwrap(), true);
+        assert_eq!(
+            is_dynamically_linked_to_libc(read("/bin/cat").unwrap()).unwrap(),
+            true
+        );
     }
     #[test]
     fn static_executable() {
-        let ld_so_filename = read_dir("/lib")
-            .unwrap()
-            .find_map(|entry| {
-                let filename = entry.unwrap().file_name();
-                let filename = filename.to_str().unwrap();
-                if filename.starts_with("ld-") {
-                    Some(filename.to_owned())
-                } else {
-                    None
-                }
-            })
-            .unwrap();
-        let ld_so_path = format!("/lib/{}", ld_so_filename);
-        assert_eq!(is_dynamically_linked_to_libc(read(ld_so_path).unwrap()).unwrap(), false);
+        let cat = read("/bin/cat").unwrap();
+        let ld_so_path = get_interp(&cat).unwrap().unwrap();
+
+        assert_eq!(
+            is_dynamically_linked_to_libc(read(OsStr::from_bytes(ld_so_path)).unwrap()).unwrap(),
+            false
+        );
     }
 }
